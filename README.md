665ec8c3e405959656adb4f3e67ba926c036d62a
Warren Krewenki <19960+krewenki@users.noreply.github.com>
2022-12-09 09:00:04 -0400

    First pass at oidc module, using community modules

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# terraform-module-template
-A template repo for terraform modules
+# terraform-aws-github-oidc
+
+This module creates an OpenID Connect provider for GitHub and an IAM role that can be assumed by GitHub Actions.
---
+++ variables.tf
@@ -0,0 +1,26 @@
+variable "role_name" {
+  description = "The name of the IAM role to create"
+  type        = string
+}
+
+variable "max_session_duration" {
+  description = "The maximum session duration (in seconds) that you want to set for the specified role. If you do not specify a value for this setting, the default maximum of one hour is applied. This setting can have a value from 1 hour to 12 hours"
+  type        = number
+  default     = 3600
+}
+
+variable "subjects" {
+  description = "A map of GitHub orgs/repositories"
+  type        = map(string)
+}
+
+variable "policies" {
+  description = "A map of IAM role names to IAM policy names"
+  type        = map(list(string))
+}
+
+variable "tags" {
+  description = "A map of tags to add to all resources"
+  type        = map(string)
+  default     = {}
+}
